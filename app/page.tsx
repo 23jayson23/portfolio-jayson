ec89a2a15e89766f874be4317491822075e76dde
23jayson23 <jreyes+euronet@koinpayments.com>
2026-01-09 02:51:24 +0800

    Add TimelineDemo component and integrate it into the homepage; update dependencies for improved performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+import { LayoutGridDemo } from "./pages/bento";
 import Header from "./pages/header";
 import Info from "./pages/info";
-import { LayoutGridDemo } from "./pages/bento";
+import { TimelineDemo } from "./pages/timeline";
 export default function Home() {
   return (
     <div className="px-5 md:px-32">
       <BackgroundRippleEffect rows={14} cols={40} />
       <Header />
       <Info />
-      <LayoutGridDemo/>
+      <LayoutGridDemo />
+      <TimelineDemo />
+      <LayoutGridDemo />
     </div>
   );
 }
